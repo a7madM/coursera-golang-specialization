aa6d1868b60095ae706d98adb932d29d4b00ec68
Ahmed Magdy <ahmadmgdi94@gmail.com>
2020-04-18 04:00:01 +0200

    Read File

---
+++ go-getting-started/src/week4/read.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+type Name struct {
+	fname string
+	lname string
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	var filename string
+	fmt.Print("Filename: ")
+	fmt.Scan(&filename)
+
+	filecontent, err := ioutil.ReadFile(filename)
+	check(err)
+
+	namesSlice := strings.Split(string(filecontent), "\n")
+	names := make([]Name, 0, len(namesSlice))
+
+	for _, v := range namesSlice {
+		n := strings.Split(v, " ")
+
+		name := Name{n[0], n[1]}
+
+		names = append(names, name)
+	}
+
+	for _, name := range names {
+		fmt.Println(name.fname, name.lname)
+	}
+}
