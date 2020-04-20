22cfd22ed024b6df01a5de56662d13444f5629d8
Ahmed Magdy <ahmadmgdi94@gmail.com>
2020-04-21 01:00:55 +0200

    Bubble Sort Week1

---
+++ go-functions/src/week1/bubblesort.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	numbers := make([]int, 0, 10)
+	fmt.Println("Type x/X to Break")
+
+	for i := 0; i < 10; i++ {
+
+		var input string
+		fmt.Print("Input: ")
+		fmt.Scan(&input)
+		if input == string('X') || input == string('x') {
+			break
+		}
+
+		number, err := strconv.Atoi(string(input))
+		if err == nil {
+			numbers = append(numbers, number)
+		} else {
+			panic(err)
+		}
+	}
+	BubbleSort(numbers)
+
+	fmt.Println(numbers)
+}
+
+// BubbleSort Function that sorts array of integer numbers
+func BubbleSort(numbers []int) {
+	length := len(numbers) - 1
+	for i := length; i > 0; i-- {
+		for x := 0; x < length; x++ {
+			if numbers[x] > numbers[x+1] {
+				numbers[x], numbers[x+1] = numbers[x+1], numbers[x]
+			}
+		}
+	}
+}
